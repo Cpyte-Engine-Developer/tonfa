6b0d00d1c50dfc1c6095775f1b419fec23d2d7fc
arman <amelikbekan0@gmail.com>
2025-11-06 21:04:28 +0300

    add getting tolerance ranges from input

--- tonfa_app.py
+++ tonfa_app.py
@@ -4,6 +4,7 @@ from kivymd.app import MDApp
 from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 from kivy.core.clipboard import Clipboard
 from kivy.metrics import dp
+from isofits import *
 
 
 class TonfaApp(MDApp):
@@ -73,4 +74,23 @@ class TonfaApp(MDApp):
         MDSnackbar(
             MDSnackbarText(text="Адрес скопирован!")
         ).open()
+
+    def fill_labels(self) -> None:
+        shaft_diameter = self.root.ids.shaft_diameter_text_field.text
+        shaft_tolerance_range_text = self.root.ids.shaft_tolerance_range_text_field.text 
+        upper_shaft_deflection_label = self.root.ids.upper_shaft_deflection_label
+        lower_shaft_deflection_label = self.root.ids.lower_shaft_deflection_label
+
+        shaft_tolerance_range = list(isotol("shaft", float(shaft_diameter), shaft_tolerance_range_text, "both"))
+        shaft_tolerance_range[0] = shaft_tolerance_range[0] / 1000
+        shaft_tolerance_range[1] = shaft_tolerance_range[1] / 1000
+
+        hole_diameter = self.root.ids.hole_diameter_text_field.text
+        hole_tolerance_range_text = self.root.ids.hole_tolerance_range_text_field.text
+        upper_hole_deflection_label = self.root.ids.upper_hole_deflection_label
+        lower_hole_deflection_label = self.root.ids.lower_hole_deflection_label
+
+        hole_tolerance_range = list(isotol("hole", float(hole_diameter), hole_tolerance_range_text, "both"))
+        hole_tolerance_range[0] = hole_tolerance_range[0] / 1000
+        hole_tolerance_range[1] = hole_tolerance_range[1] / 1000
 
