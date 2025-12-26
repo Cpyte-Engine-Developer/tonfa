d45583885a85bf68e314cbf5fc1f899624e795f3
arman <amelikbekan0@gmail.com>
2025-12-26 20:42:21 +0300

    update version 2

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ docker run --interactive --tty --rm \
     --volume "$PWD":/home/user/hostcwd \
     kivy/buildozer android release
 
-zipalign -v 4 ./bin/tonfa-0.0.1-arm64-v8a-release-unsigned.apk ./bin/tonfa-0.0.1-arm64-v8a-release-signed.apk
-apksigner sign --ks ~/keystores/tonfa.keystore --ks-key-alias tonfa bin/tonfa-0.0.1-arm64-v8a-release-signed.apk
+zipalign -v 4 ./bin/tonfa-0.0.2-arm64-v8a-release-unsigned.apk ./bin/tonfa-0.0.2-arm64-v8a-release-signed.apk
+apksigner sign --ks ~/keystores/tonfa.keystore --ks-key-alias tonfa bin/tonfa-0.0.2-arm64-v8a-release-signed.apk
